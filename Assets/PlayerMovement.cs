e1fc3c6055647783045ae9570eab133f306cf978
MarcioMarchiori <marciomarchiori@yahoo.com.br>
2016-11-04 00:14:39 -0200

    T

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLenght = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move (h, v);
+        Turning();
+    }
+
+    void Move (float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+            if(Physics.Raycast (camRay, out floorHit, camRayLenght, floorMask))
+            {
+                Vector3 playerToMouse = floorHit.point - transform.position;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+            }
+    }
+        
+    
+	}
+
